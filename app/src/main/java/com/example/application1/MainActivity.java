befd3051396a1045bae6442b3200d054e7f2298d
cv33301 <a0983360956@gmail.com>
2024-05-27 16:32:24 +0800

    no message

--- app/src/main/java/com/example/application1/MainActivity.java
+++ app/src/main/java/com/example/application1/MainActivity.java
@@ -15,8 +15,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
           //setContentView(R.layout.activity_main);
-          setContentView(R.layout.activity_relativelayout2);
-          //setContentView(R.layout.activity_edittext);
+          setContentView(R.layout.actiuity_gridlayout_special);
 //        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
 //            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
 //            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
