ee3f49b2b4c575d7d23f765b811c38f95b9ace17
cv33301 <a0983360956@gmail.com>
2024-06-05 16:43:19 +0800

    no message

---
+++ app/src/main/java/com/example/application1/Activity_Button_Java.java
@@ -0,0 +1,39 @@
+package com.example.application1;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Activity_Button_Java extends AppCompatActivity {
+
+    Button btn_01;
+    int i = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_button_java);
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//            return insets;
+//        });
+
+        btn_01 = findViewById(R.id.button_01);
+
+        btn_01.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public  void  onClick(View v) {
+                i = i + 1;
+                btn_01.setText("已按下按鈕"+i+"次");
+            }
+        });
+    }
+}
